49e2d2be4b8ff18e3b8da10abc1ac25f0a4f62a7
Fowmiya <fowmiya7411@gmail.com>
2026-09-19 14:18:26 +0530

    Fix Vercel API routing

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,13 @@
 const app = require("../server");
 
-module.exports = app;
+module.exports = (req, res) => {
+  const originalUrl = req.url;
+
+  if (originalUrl === "/") {
+    req.url = "/";
+  } else if (!originalUrl.startsWith("/api")) {
+    req.url = `/api${originalUrl}`;
+  }
+
+  return app(req, res);
+};
